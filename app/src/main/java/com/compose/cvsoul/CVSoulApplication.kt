c4814510d617d4b8a2484d206c0b61e75b91f082
zerokarafont <735654351@qq.com>
2021-08-03 11:46:28 +0800

    fix: 数据解密jsonParse

--- app/src/main/java/com/compose/cvsoul/CVSoulApplication.kt
+++ app/src/main/java/com/compose/cvsoul/CVSoulApplication.kt
@@ -4,14 +4,21 @@ import android.annotation.SuppressLint
 import android.app.Application
 import android.content.Context
 import android.os.Build
+import android.util.Log
 import com.compose.cvsoul.util.Response
 import com.compose.cvsoul.util.crypto.getRawBase64Key
 import com.compose.cvsoul.util.crypto.getSessionId
 import com.compose.cvsoul.util.crypto.getToken
 import com.compose.cvsoul.util.getDefaultOkHttpClient
+import com.google.gson.JsonElement
+import com.google.gson.JsonParser
+import com.google.gson.annotations.JsonAdapter
 import com.soywiz.krypto.AES
 import com.soywiz.krypto.Padding
 import com.soywiz.krypto.encoding.Base64
+import io.vertx.core.json.Json
+import io.vertx.core.json.JsonObject
+import io.vertx.core.spi.json.JsonCodec
 import rxhttp.RxHttpPlugins
 import rxhttp.wrapper.cahce.CacheMode
 import rxhttp.wrapper.utils.GsonUtil
@@ -39,11 +46,11 @@ class CVSoulApplication: Application() {
 //            .setExcludeCacheKeys(String...)
             //设置数据解密/解码器
             .setResultDecoder { encryptData ->
-                val response = GsonUtil.fromJson<Response<String>>(encryptData, Response::class.java)
+                val response = GsonUtil.fromJson<Response<Any>>(encryptData, Response::class.java)
                 if (response.data != null) {
                     val key = getRawBase64Key()?.let { Base64.decode(it) }
-                    val data = String(AES.decryptAes128Cbc(Base64.decode(response.data!!), key!!, Padding.PKCS7Padding))
-                    response.data = data
+                    val data = String(AES.decryptAes128Cbc(Base64.decode(response.data!! as String), key!!, Padding.PKCS7Padding))
+                    response.data = JsonParser.parseString(data)
                     GsonUtil.toJson(response)
                 }else {
                     encryptData
