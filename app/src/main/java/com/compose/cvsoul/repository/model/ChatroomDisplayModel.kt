74d99b8bf60a7b9a42a470c77eea234002d16076
zerokarafont <735654351@qq.com>
2021-08-05 22:46:33 +0800

    feat: 定义聊天室 data model

---
+++ app/src/main/java/com/compose/cvsoul/repository/model/ChatroomDisplayModel.kt
@@ -0,0 +1,9 @@
+package com.compose.cvsoul.repository.model
+
+/**
+ * 聊天室
+ * @param _id
+ * @param cover 封面
+ * @param title 标题
+ */
+data class ChatroomDisplayModel(val _id: String, val cover: String, val title: String)
